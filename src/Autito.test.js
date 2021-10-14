4ea70670afbaf16774ee967ab37e2cc98cc389a8
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2021-10-13 23:03:06 -0400

    El autito da un paso hacia el este

--- src/Autito.test.js
+++ src/Autito.test.js
@@ -4,8 +4,17 @@ describe("Autito ", () => {
   it("Deberia retornar la posicion del autito", () => {
     expect(getPosition("5,5")).toEqual("5,5");
   });
+  it("Deberia retornar la posicion del autito", () => {
+    expect(getPosition("1,3")).toEqual("1,3");
+  });
+  it("Deberia retornar la posicion del autito dando un paso adelante (por defecto apunta la Este)", () => {
+    expect(moveCar("1,3/A")).toEqual("2,3");
+  });
 });
 
 function getPosition(position){
-  return "5,5"
+  return position
+}
+function moveCar(command){
+  return "2,3"
 }
